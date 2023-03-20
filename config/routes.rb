a04b43f6896e1a2b1183cb14b4ae35bb0a8f21cb
Chieri Kii <kiichieri@gmail.com>
2023-03-20 18:00:58 +0900

    model setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'lists/index'
+  # get 'lists/index'
   root to: 'lists#index'
+  # root to: "pages#home"
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
